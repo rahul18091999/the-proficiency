2b35cc23ca648785b9705ed34fca18ad4dec5ca4
rahul18091999 <rahulmittalnrw@gmail.com>
2020-04-19 20:48:08 +0530

    add teacher

--- teacher/views.py
+++ teacher/views.py
@@ -8,7 +8,7 @@ def getpass(a):
     for i in a:
         h=chr(ord(i)-10 if(ord(i)>65 and ord(i)<90) else ord(i)+26)
         p+=str(ord(i))+h
-    return b64encode(p.encode())
+    return str(b64encode(p.encode()))
 def teacher(request):
     if request.method == "POST":
         print('rahul')
@@ -20,9 +20,6 @@ def teacher(request):
         gen=request.POST.get('gen')
         s=request.POST.get('s')
         d=request.POST.get('d')
-        a=number+"@TP@"+age
-        password = getpass(a)
-        print(s,d)
         data = {
                 'name':name,
                 'number':number,
@@ -37,10 +34,62 @@ def teacher(request):
             error="Name is invalid"
             data['error']=error
             return render(request,'teacher.html',data)
+        elif(len(str(number))!=10 ):
+            error="Phone Number is invalid"
+            data['error']=error
+            return render(request,'teacher.html',data)
+        elif(email==''):
+            error="Email is invalid"
+            data['error']=error
+            return render(request,'teacher.html',data)
         elif(s is None):
             error = "Please Select State"
             data['error']=error
             return render(request,'teacher.html',data)
+        if (database.child('tIds').child(number).shallow().get().val()):
+            error = "Phone Number Already exists"
+            data['error']=error
+            return render(request,'teacher.html',data)
+        else:
+            if(database.child('tIds').child('free').shallow().get().val()):
+                tempid=database.child('tIds').child('free').shallow().get().val()
+            else:
+                tempid=100001
+            import time
+            from datetime import datetime,timezone
+            import pytz
+            from random import randint
+            print()
+            
+            time_now=int(datetime.now().timestamp()*100)
+            
+            print(time_now)
+            database.child('tIds').child(number).update({
+                'createsOn':time_now,
+                'id':"12"+str(tempid),
+                'verify':randint(100000,999999),
+                'pass':getpass(number+"@TP@"+age)[2:-1]
+            })
+            database.child('teachers').child("12"+str(tempid)).child('details').update(
+                {
+                    'name': name,
+                    'age': age,
+                    'state': s,
+                    'city': d,
+                    'experience': experience,
+                    'phone': number,
+                    'email': email,
+                    'gen': gen
+                }
+            )
+            database.child('tIds').update({'free':tempid+1})
+            data = {
+                'name':'',
+                'email':'',
+                
+        }
+            data['success']="Teacher has been added Successfully!"
+            data['info']= number + "@TP@" + age + " and 12" + str(tempid) + " is the Password and ID for " + ("Mr. " if gen == 'Male' else "Ms. ")+ name 
         return render(request,'teacher.html',data)
     else:    
         name=""
