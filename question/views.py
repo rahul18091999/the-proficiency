e31f37e1613627e4a414eab513e329cf22253591
harshr78 <harshrawal@gmail.com>
2020-04-21 02:08:32 +0530

    change in api

--- question/views.py
+++ question/views.py
@@ -2,23 +2,33 @@ from django.shortcuts import render
 from exam.views import database
 # Create your views here.
 
-def question(request):
-    subjectData=database.child('subjects').get()
-    subjectid=[]
-    topicName=[]
-    k=0
-    for i in subjectData:
-        if(i.key()!='free'):
-            subjectid.append({'id':i.key(),'name':i.val()['details']['name'],'index':k})
-            
-            topic=i.val()['topics']
-            t=[]
-            for j in topic:    
-                if(j!='free'):
-                    t.append({'id':j,'name':topic[j]['details']['name']})
-            
-            topicName.append(t)
-            k+=1
-    print(topicName)
 
-    return render(request,'addQues.html',{'data':subjectid,'topic':topicName})
+def question(request):
+    subjectData = database.child('subjects').get()
+    teacherData = database.child('teachers').get()
+    subjectid = []
+    topicName = []
+    teacher = []
+    k = 0
+    for i in subjectData:
+        if(i.key() != 'free'):
+            subjectid.append({'id': i.key(), 'name': i.val()[
+                'details']['name'], 'index': k})
+
+            topic = i.val()['topics']
+            t = []
+            for j in topic:
+                if(j != 'free'):
+                    t.append({'id': j, 'name': topic[j]['details']['name']})
+                    topicName.append(t)
+                    k += 1
+
+    for i in teacherData:
+        if (i.key() != 'qBank'):
+            teacher.append(
+                {
+                    'tId': i.key(),
+                    'name': i.val()['details']['name']
+                }
+            )
+    return render(request, 'addQues.html', {'data': subjectid, 'topic': topicName, 'teach': teacher})
