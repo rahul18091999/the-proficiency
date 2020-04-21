830b3de1c7a2e1b8122410f0204aa942c311ad8b
harshr78 <harshrawal@gmail.com>
2020-04-21 07:45:50 +0530

    change in api

--- question/views.py
+++ question/views.py
@@ -6,14 +6,15 @@ from exam.views import database
 def question(request):
     subjectData = database.child('subjects').get()
     teacherData = database.child('teachers').get()
-    data=[]
+    data = []
     subjectid = []
     topicName = []
     teacher = []
     k = 0
     for i in subjectData:
         if(i.key() != 'free'):
-            subjectid.append({'id': i.key(), 'name': i.val()['details']['name'], 'index': k})
+            subjectid.append({'id': i.key(), 'name': i.val()[
+                             'details']['name'], 'index': k})
 
             topic = i.val()['topics']
             t = []
@@ -30,7 +31,7 @@ def question(request):
                     'tId': i.key(),
                     'name': i.val()['details']['name']
                 }
-            )         
+            )
 
     if request.method == "POST":
         ques = request.POST.get('ques')
@@ -43,70 +44,58 @@ def question(request):
         subject = request.POST.get('subject')
         topic = request.POST.get('topic')
         print(optc)
-       
-        if(ques!="" and opt1!="" and opt2!="" and opt3!="" and opt4!="" and optc is not None and subject is not None and teach is not None and topic is not None):
-            pass
+
+        if(ques != "" and opt1 != "" and opt2 != "" and opt3 != "" and opt4 != "" and optc is not None and subject is not None and teach is not None and topic is not None):
+
+            free = database.child('questions').child(
+                'free').shallow().get().val()
+            if free:
+                tempid = free
+            else:
+                tempid = 1000000001
+            if teach == "qBank":
+                check = "true"
+            else:
+                check = "false"
+            database.child('questions').child("q"+str(tempid)).child('details').update(
+                {   
+                    'approved': check,
+                    'by': teach,
+                    'question': ques,
+                    'opt1': opt1,
+                    'opt2': opt2,
+                    'opt3': opt3,
+                    'opt4': opt4,
+                    'teacher': teach,
+                    'subject': subject,
+                    'topic': topic,
+                }
+            )
+            database.child('teachers').child(teach).child('questions').update({'topic': topic})
+            database.child('questions').update({'free':tempid+1})
+            data={
+                'question': "",
+                'opt1': "",
+                'opt2': "",
+                'opt3': "",
+                'opt4': "",
+                'teacher': "",
+                'subject': "",
+                'topic': "",
+                'success': 'data submitted successfully',
+            }
+            return render(request,"addQues.html",{'subject': subjectid, 'topic': topicName, 'teach': teacher, 'data': data})
         else:
-             data = {
-            'question': ques,
-            'opt1': opt1,
-            'opt2': opt2,
-            'opt3': opt3,
-            'opt4': opt4,
-            'teacher': teach,
-            'subject': subject,
-            'topic': topic,
-            'error':'Please check all the Details again.',
-            }   
-            
+            data = {
+                'question': ques,
+                'opt1': opt1,
+                'opt2': opt2,
+                'opt3': opt3,
+                'opt4': opt4,
+                'teacher': teach,
+                'subject': subject,
+                'topic': topic,
+                'error': 'Please check all the Details again.',
+            }
+    return render(request, 'addQues.html', {'subject': subjectid, 'topic': topicName, 'teach': teacher, 'data': data})
 
-    return render(request, 'addQues.html', {'subject': subjectid, 'topic': topicName, 'teach': teacher,'data':data})
-
-
-def addquestion(request):
-    if request.method == "POST":
-        ques = request.POST.get('ques')
-        opt1 = request.POST.get('opt1')
-        opt2 = request.POST.get('opt2')
-        opt3 = request.POST.get('opt3')
-        opt4 = request.POST.get('opt4')
-        teacher = request.POST.get('teacher')
-        subject = request.POST.get('subject')
-        topic = request.POST.get('topic')
-
-        data = {
-            'question': ques,
-            'opt1': opt1,
-            'opt2': opt2,
-            'opt3': opt3,
-            'opt4': opt4,
-            'teacher': teacher,
-            'subject': subject,
-            'topic': topic,
-        }
-        if (ques == "" ):
-            error = "pllease fill the below"
-            data['error'] = error
-            return render(request, 'addQues.html' , data)
-        elif (opt1 == "" ):
-            error = "pllease fill the below"
-            data['error'] = error
-            return render(request, 'addQues.html' , data)
-        elif (opt3 == "" ):
-            error = "pllease fill the below"
-            data['error'] = error
-            return render(request, 'addQues.html' , data)
-        elif (opt3 == "" ):
-            error = "pllease fill the below"
-            data['error'] = error
-            return render(request, 'addQues.html' , data)
-        elif (opt4 == "" ):
-            error = "pllease fill the below"
-            data['error'] = error
-            return render(request, 'addQues.html' , data)
-        elif ( teacher ==None or subject == None or topic == None):
-            error = "please select the below"
-            data['error'] = error
-            return render(request, 'addQues.html',data)
-        else:
-            return render(request, 'addQues.html')
