ed9d6b9c3bb6480cf207244fc6e6ac4065645bdb
harshr78 <harshrawal@gmail.com>
2020-04-24 20:34:15 +0530

    change in api

--- typers/views.py
+++ typers/views.py
@@ -8,35 +8,20 @@ def dashboard(request):
 def viewQues(request, ide):
     user = request.session['us']
     if ide == '1':
-        if user == 14:
-            iduser = request.session['user']
-            typerdata = database.child('typers').child(iduser).child('questionsAdded').get()
-            print(typerdata.val())
-            l = []
-            for i in typerdata:
-                # id = i.val()['questionsAdded']['by']
-                # if id == iduser:
-                l.append(
-                    {
-                        'id': i.key(),
-                        'by': iduser,
-                    }
-                )
-            return render(request, 'viewQuestyper.html', {'question': l})
-        else:
-            typerdata = database.child('typers').child(ide).child('questionsAdded').get()
-            print(typerdata.val())
-            l = []
-            for i in typerdata:
-                # id = i.val()['questionsAdded']['by']
-                # if id == iduser:
-                l.append(
-                    {
-                        'id': i.key(),
-                        'by': iduser,
-                    }
-                )
-            return render(request, 'viewQuestyper.html', {'question': l})
+        iduser = request.session['user']
+        typerdata = database.child('typers').child(iduser).child('questionsAdded').get()
+        print(typerdata.val())
+        l = []
+        for i in typerdata:
+            # id = i.val()['questionsAdded']['by']
+            # if id == iduser:
+            l.append(
+                {
+                    'id': i.key(),
+                    'by': iduser,
+                }
+            )
+        return render(request, 'viewQuestyper.html', {'question': l})
     else:
         typerdata = database.child('typers').child(ide).child('questionsAdded').get()
         print(typerdata.val())
