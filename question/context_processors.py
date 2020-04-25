68a376bbc0becacc97a7ab7abcb06f88443a27e1
harshr78 <harshrawal@gmail.com>
2020-04-25 21:57:00 +0530

    change in api

--- question/context_processors.py
+++ question/context_processors.py
@@ -6,13 +6,17 @@ def add_variable_to_context(request):
         
         
         try:
+            ses = request.session['us']
+            image=request.session['image']
+            name=request.session['name']
             path=request.GET.get('id')
             return {
-                'session': ses,'image':image,'name':name,'subid':path[:2]
+            'session': ses,'image':image,'name':name,'subid':path[:2]
             }
         except:
-            return {
-                'session': ses,'image':image,'name':name,'subid':path[:2]
-            }
+            pass
+        # return {
+        #     'session': ses,'image':image,'name':name,'subid':path[:2]
+        # }
     except:
         return {}
