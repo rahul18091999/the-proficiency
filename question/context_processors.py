19be4fd60ace57dc22f70179bbf16a11475c3524
dhandag <groupsdhanda@gmail.com>
2020-04-25 19:54:52 +0530

    Edit Profile

--- question/context_processors.py
+++ question/context_processors.py
@@ -1,26 +1,15 @@
 def add_variable_to_context(request):
     try:
         ses = request.session['us']
-        image=request.session['image']
-        name=request.session['name']
-        
-        
-        try:
-            path=request.GET.get('id')
-<<<<<<< HEAD
-            return {
-                'session': ses,'image':image,'name':name,'subid':path[:2]
-=======
+        image = request.session['image']
+        name = request.session['name']
+
+        if(request.path.find('id')):
+            path = request.GET.get('id')
         else:
-            path='999'
+            path = '999'
         return {
-            'session': ses,'homeimage':image,'homename':name,'subid':str(path)[:2]
->>>>>>> fb176d3e064a86fbd1aa3c2a74719a303053c323
-            }
-        except:
-            pass
-        return {
-            'session': ses,'image':image,'name':name
+            'session': ses, 'homeimage': image, 'homename': name, 'subid': str(path)[:2]
         }
     except:
         return {}
--- typers/views.py
+++ typers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from exam.views import checkpermission, database
 from datetime import date
 
+
 def dashboard(request):
     id = request.session['user']
     typersquestion = database.child('typers').child(
@@ -14,41 +15,8 @@ def dashboard(request):
 
 def viewQues(request):
     iduser = request.session['user']
-<<<<<<< HEAD
-    typerdata = database.child('typers').child(
-        iduser).child('questionsAdded').get()
-    print(typerdata.val())
-    l = []
-    for i in typerdata:
-        l.append(
-            {
-                'id': i.key(),
-                'by': iduser,
-            }
-        )
-    return render(request, 'viewQuestyper.html', {'question': l})
-
-
-def editProfile(request):
-    iduser = request.session['user']
-    i = database.child('typers').child(iduser).child('details').get()
-    l = {
-        'id': iduser,
-        'name': i.val()["name"],
-        'age':i.val()["age"],
-        'city':i.val()["city"],
-        'email':i.val()["email"],
-        'experience':i.val()["experience"],
-        'gen':i.val()["gen"],
-        'phone':i.val()["phone"],
-        'state':i.val()["state"],
-        'createdOn':database.child('tyIds').child(i.val()["phone"]).child('createdOn').get().val()
-    }
-    return render(request, './typer/editProfile.html', {'data': l})
-=======
     typerdata = database.child('typers').child(iduser).child('questionsAdded').get()
     questions = database.child('questions').get()
-    print(typerdata.val())
     l = []
     for i in typerdata:
         for j in questions:
@@ -61,4 +29,21 @@ def editProfile(request):
                     }
                 )
     return render(request, 'viewQuestyper.html', {'question': l})
->>>>>>> fb176d3e064a86fbd1aa3c2a74719a303053c323
+
+
+def editProfile(request):
+    iduser = request.session['user']
+    i = database.child('typers').child(iduser).child('details').get()
+    l = {
+        'id': iduser,
+        'name': i.val()["name"],
+        'age': i.val()["age"],
+        'city': i.val()["city"],
+        'email': i.val()["email"],
+        'experience': i.val()["experience"],
+        'gen': i.val()["gen"],
+        'phone': i.val()["phone"],
+        'state': i.val()["state"],
+        'createdOn': database.child('tyIds').child(i.val()["phone"]).child('createdOn').get().val()
+    }
+    return render(request, './typer/editProfile.html', {'data': l})
