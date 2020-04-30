ba81eee44dc858e59bf7e5629dd8381be341aa8a
rahul18091999 <rahulmittalnrw@gmail.com>
2020-04-30 09:39:15 +0530

    pull

--- teacher/views.py
+++ teacher/views.py
@@ -405,22 +405,25 @@ def editProfile(request):
 def referal(request):
     idd = request.session['user']
     data = database.child('share').child('teachers').child(idd).get()
-    totalearning = data.val()['earned']
-    typea = data.val()['typeA']
-    typeb = data.val()['typeB']
-    typec = data.val()['typeC']
-    code = {'typea': typea['code'],
-            'typeb': typeb['code'], 'typec': typec['code']}
-    typeatrnc = []
-    typebtrnc = []
-    typectrnc = []
-    if 'trnc' in typea:
-        for i in typea['trnc']:
-            typeatrnc.append({'trnc': i, 'earn': typea['trnc'][i]})
-    if 'trnc' in typeb:
-        for i in typeb['trnc']:
-            typebtrnc.append({'trnc': i, 'earn': typeb['trnc'][i]})
-    if 'trnc' in typec:
-        for i in typec['trnc']:
-            typectrnc.append({'trnc': i, 'earn': typec['trnc'][i]})
-    return render(request, './teacher/referal.html', {'code': code, 'typeatrnc': typeatrnc, 'typebtrnc': typebtrnc, 'typectrnc': typectrnc})
+    if data.val():
+        totalearning = data.val()['earned']
+        typea = data.val()['typeA']
+        typeb = data.val()['typeB']
+        typec = data.val()['typeC']
+        code = {'typea': typea['code'],
+                'typeb': typeb['code'], 'typec': typec['code']}
+        typeatrnc = []
+        typebtrnc = []
+        typectrnc = []
+        if 'trnc' in typea:
+            for i in typea['trnc']:
+                typeatrnc.append({'trnc': i, 'earn': typea['trnc'][i]})
+        if 'trnc' in typeb:
+            for i in typeb['trnc']:
+                typebtrnc.append({'trnc': i, 'earn': typeb['trnc'][i]})
+        if 'trnc' in typec:
+            for i in typec['trnc']:
+                typectrnc.append({'trnc': i, 'earn': typec['trnc'][i]})
+
+        return render(request, './teacher/referal.html', {'code': code, 'typeatrnc': typeatrnc, 'typebtrnc': typebtrnc, 'typectrnc': typectrnc})
+    return (request,'./teacher/referal.html')
