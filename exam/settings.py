2bfacce38377e5bc145b871bd2a3838f349a4f3f
dhandag <groupsdhanda@gmail.com>
2020-05-01 13:38:58 +0530

    hello

--- exam/settings.py
+++ exam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g*ej$5wzhp20du5(-p)b$k2e5f)xbz5r=^fnmtzs8a%jfw%68@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
