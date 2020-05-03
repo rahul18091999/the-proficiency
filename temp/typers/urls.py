97b8e36b5a38ae248332d0dcb059a5abff7fc2d7
dhandag <groupsdhanda@gmail.com>
2020-05-03 19:49:58 +0530

    Rermoval of unwanted files

--- temp/typers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('viewquestion', views.viewQues),
-    path('dashboard',views.dashboard),
-    path('editProfile', views.editProfile),
-]
