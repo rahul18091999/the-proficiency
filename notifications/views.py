c0fd03571f7718e8ffdf6c0a3e1508c589207a30
rahul18091999 <rahulmittalnrw@gmail.com>
2020-05-06 22:56:20 +0530

    push

--- notifications/views.py
+++ notifications/views.py
@@ -3,7 +3,20 @@ from exam.views import database
 # Create your views here.
 
 def viewNotifications(request):
-    return render(request,'./notification/viewNotifications.html')
+    d = database.child('notifications').get().val()
+    l=[]
+    if d:
+        from datetime import datetime
+        for i in d:
+            if i !='free':
+                print(d[i])
+                l.append({
+                    'nid':i,
+                    'time':datetime.fromtimestamp(d[i]['time']/100),
+                    'title':d[i]['title'],
+                    'to':d[i]['to']
+                })
+    return render(request,'./notification/viewNotifications.html',{'data':l})
 
 
 def addNotifications(request):
@@ -12,52 +25,61 @@ def addNotifications(request):
         desc = request.POST.get('discription')
         to = request.POST.get('to')
         print(to)
-        data = database.child(to).get().val()
-        token=[]
-        # print(data)
-        if data:
-            for j in data:
-                if 'notifications' in data[j]:
-                    token.append(data[j]['notifications']['token'])
-        print(token)
-        import requests
-        r = requests.post('https://exp.host/--/api/v2/push/send',
-        headers={
-            "HTTP_ACCEPT":'application/json',
-            "HTTP_ACCEPT_ENCODING":'gzip, deflate',
-            "HTTP_HOST":'the-proficiency.com',
-            'Content-type': 'application/json'
-        },
-        json={
-            'to':token,                        
-              'title': title,                  
-              'body': desc,             
-              'priority': "high",            
-              'sound':"default",              
-              'channelId':"default",   
+        
+        if(title and desc and to):
+            data = database.child(to).get().val()
+            token=[]
+            td = data
+            from datetime import datetime
+            time_now = int(datetime.now().timestamp()*100)
+            print(time_now)
+            # if data:
+            #     temp = database.child('notifications').child('free').shallow().get().val()
+            #     if temp:
+            #         idd = temp
+            #     else:
+            #         idd = 100000000000000
+            #     print(temp)
+            #     for j in data:
+                    
+            #         if 'notifications' in data[j]:
+            #             td[j]['notifications']['notes']={idd:time_now}
+            #             token.append(data[j]['notifications']['token'])
+            #         else:
+            #             td[j]['notifications']={}
+            #             td[j]['notifications']['notes']={idd:time_now}
+            # if token:
+            #     print(token)
+            #     token.append('EfadsfadsfasdfasdxponentPushfasdTofsadfskfadssdafsdfen[4iSHToD76BENf7-ujv4hcN')
+            #     import requests
+            #     r = requests.post('https://exp.host/--/api/v2/push/send',
+            #     headers={
+            #         "HTTP_ACCEPT":'application/json',
+            #         "HTTP_ACCEPT_ENCODING":'gzip, deflate',
+            #         "HTTP_HOST":'the-proficiency.com',
+            #         'Content-type': 'application/json'
+            #     },
+            #     json={
+            #         'to':token,                        
+            #           'title': title,                  
+            #           'body': desc,             
+            #           'priority': "high",            
+            #           'sound':"default",              
+            #           'channelId':"default",   
 
-        })
-        print(r.text)
-#         fetch('https://exp.host/--/api/v2/push/send', {       
-#          method: 'POST', 
-#          headers: {
-#                Accept: 'application/json',  
-#               'Content-Type': 'application/json', 
-#               'accept-encoding': 'gzip, deflate',   
-#               'host': 'exp.host'      
-#           }, 
-#         body: JSON.stringify({                 
-#               to: token,                        
-#               title: 'New Notification',                  
-#               body: 'The notification worked!',             
-#               priority: "high",            
-#               sound:"default",              
-#               channelId:"default",   
-#                   }),        
-#       }).then((response) => response.json())   
-#                .then((responseJson) => {  })
-#                       .catch((error) => { console.log(error) });
-# }
+            #     })
+            #     import ast
+            #     d = ast.literal_eval(r.text)['data']
+            #     for i in d:
+            #         print (i['status'])
+        else:
+            data={
+                'title':title,
+                'desc':desc,
+                'to':to,
+                'error':"Please fill all the details."
+            }
+            return render(request,'./notification/addNotifications.html',{'data':data})
     return render(request,'./notification/addNotifications.html')
 
 
